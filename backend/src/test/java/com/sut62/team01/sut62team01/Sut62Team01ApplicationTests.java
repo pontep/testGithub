dc35c111afae1afa41388b71a7b19a26b2c006a9
dincracker <pontep.taveeshup@gmail.com>
2019-12-01 23:54:25 +0700

    init springboot - close #35

---
+++ backend/src/test/java/com/sut62/team01/sut62team01/Sut62Team01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sut62.team01.sut62team01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sut62Team01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
