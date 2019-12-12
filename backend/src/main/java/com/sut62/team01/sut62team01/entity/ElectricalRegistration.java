8228b3a5cc5b0acba101c67ba4e5f43704c3a1ab
unknown <pummarinB6003296@gmail.com>
2019-12-12 22:56:10 +0700

    fix code a little bit

--- backend/src/main/java/com/sut62/team01/sut62team01/entity/ElectricalRegistration.java
+++ backend/src/main/java/com/sut62/team01/sut62team01/entity/ElectricalRegistration.java
@@ -26,30 +26,29 @@ public class ElectricalRegistration{
     @SequenceGenerator(name = "ElectricalRegistration_seq", sequenceName = "ElectricalRegistration_seq")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="ElectricalRegistration_seq")
     @Column(name = "ElectricalRegistration_ID", unique = true, nullable = true,insertable = true)    
-    private @NonNull long id;
-
-    private @NonNull Date ElecticalRegistrationdate;
+    private @NotNull long id;
+    private @NotNull Date electicalRegistrationdate;
 
     @OneToOne(fetch = FetchType.EAGER, targetEntity = RoomBooking.class)
     @JoinColumn(name = "ROOMBOOKING_ID", insertable = true)
     @JsonManagedReference
-    private @NonNull RoomBooking roomBooking;
+    private @NotNull RoomBooking roomBooking;
 
     @ManyToOne(fetch = FetchType.EAGER, targetEntity = Type.class)
     @JoinColumn(name = "TYPE_ID", insertable = true)
     @JsonManagedReference
-    private @NonNull Type Type;
+    private @NotNull Type Type;
 
     @OneToOne(fetch = FetchType.EAGER, targetEntity = Staff.class)
     @JoinColumn(name = "STAFF_ID", insertable = true)
     @JsonManagedReference
-    private @NonNull Staff Staff;
+    private @NotNull Staff Staff;
 
     public ElectricalRegistration(){}
     public ElectricalRegistration(RoomBooking rb, Type t, Staff st){
         this.roomBooking = rb;
         this.Type = t;
         this.Staff = st;
-        this.ElecticalRegistrationdate = new Date();
+        this.electicalRegistrationdate = new Date();
     }
 }
